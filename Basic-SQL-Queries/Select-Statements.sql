f423d94478f4e7ee2c7312d3522a12558c9a2e30
Ralitsa <ralitsa.petkova01@gmail.com>
2026-06-21 17:10:38 +0300

    Update Select-Statements.sql

--- Basic-SQL-Queries/Select-Statements.sql
+++ Basic-SQL-Queries/Select-Statements.sql
@@ -1,1 +1,15 @@
+SELECT * FROM Orders.Suppliers;
+GO
 
+SELECT TOP 1 * FROM Orders.Suppliers;
+GO
+
+SELECT TOP 1000 * FROM Orders.Suppliers;
+GO
+
+SELECT Suppliers.SupplierName,
+    Suppliers.SupplierCategoryID AS Category,
+    Suppliers.PrimaryContactPersonID AS PrimaryContact,
+    Suppliers.AlternateContactPersonID AS SecondaryContact,
+    Suppliers.DeliveryMethodID
+FROM Orders.Suppliers;
