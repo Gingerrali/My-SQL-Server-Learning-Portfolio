c0d64c6f3456630c3bba611a4e1f4f6a6da2024d
Ralitsa <ralitsa.petkova01@gmail.com>
2026-06-22 20:23:41 +0300

    Update Where-Clause.sql

--- Basic-SQL-Queries/Where-Clause.sql
+++ Basic-SQL-Queries/Where-Clause.sql
@@ -1,5 +1,16 @@
 SELECT * FROM Orders.Sales
 WHERE PickedByPersonID > 0;
-
+GO
+  
 SELECT * FROM Orders.Sales
 WHERE SalespersonPersonID BETWEEN 0 AND 10;
+GO
+  
+SELECT OrderID, 
+  CustomerID, 
+  SalespersonPersonID, 
+  OrderDate
+FROM Orders.Sales
+WHERE SalespersonPersonID BETWEEN 0 AND 10
+AND OrderDate = '2013-01-01';
+GO
