d4849bf1cce5d900dcb84a786f8a9c8484fa0184
Ralitsa <ralitsa.petkova01@gmail.com>
2026-06-23 17:28:03 +0300

    Update 4-Aggregate-Functions.sql

--- Basic-SQL-Queries/4-Aggregate-Functions.sql
+++ Basic-SQL-Queries/4-Aggregate-Functions.sql
@@ -1,4 +1,14 @@
--- 
+-- Calculates a total using SUM()
 SELECT SUM(SalespersonPersonID) AS TotalSales
 FROM Orders.Sales;
+GO
+
+-- Counts suppliers per payment period
+-- Shows only payment periods used by more than one supplier
+SELECT PaymentDays,
+  COUNT(*) AS TotalSuppliers
+FROM Orders.Suppliers
+GROUP BY PaymentDays
+HAVING COUNT(*) > 1
+ORDER BY TotalSuppliers DESC;
 GO
