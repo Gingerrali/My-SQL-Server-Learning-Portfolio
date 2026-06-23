242beaa6fb0e77a86d36b21687e68f4150119486
Ralitsa <ralitsa.petkova01@gmail.com>
2026-06-23 17:29:10 +0300

    Update 4-Aggregate-Functions.sql

--- Basic-SQL-Queries/4-Aggregate-Functions.sql
+++ Basic-SQL-Queries/4-Aggregate-Functions.sql
@@ -12,3 +12,11 @@ GROUP BY PaymentDays
 HAVING COUNT(*) > 1
 ORDER BY TotalSuppliers DESC;
 GO
+
+-- MAX, MIN, AVG aggregate functions example
+SELECT
+    MAX(PaymentDays) AS LongestPeriodRemaining,
+    MIN(PaymentDays) AS ShortestPeriodRemaining,
+    AVG(PaymentDays) AS AverageRemainingTime
+FROM Orders.Suppliers;
+GO
