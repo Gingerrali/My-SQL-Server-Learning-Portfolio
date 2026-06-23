8d40e2d565bf03b778fb1a7a2281adf9560ddb2a
Ralitsa <ralitsa.petkova01@gmail.com>
2026-06-23 13:36:50 +0300

    Update Where-Clause.sql

--- Basic-SQL-Queries/Where-Clause.sql
+++ Basic-SQL-Queries/Where-Clause.sql
@@ -15,6 +15,10 @@ WHERE SalespersonPersonID BETWEEN 0 AND 10
 AND OrderDate = '2013-01-01';
 GO
 
+SELECT * FROM Orders.Sales
+WHERE SalespersonPersonID IN (2, 7, 8);
+GO
+  
 SELECT * FROM Orders.Suppliers
 WHERE SupplierCategoryID = 2
 OR SupplierCategoryID = 3;
