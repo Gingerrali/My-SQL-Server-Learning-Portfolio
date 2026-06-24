0fb24d5d3f34f9a345a0d5e1f7f17be2e1d16946
Ralitsa <ralitsa.petkova01@gmail.com>
2026-06-24 18:32:43 +0300

    Update 2-Scalar-Valued-Functions.sql

--- User-Defined-Functions/2-Scalar-Valued-Functions.sql
+++ User-Defined-Functions/2-Scalar-Valued-Functions.sql
@@ -1,1 +1,33 @@
+CREATE FUNCTION dbo.fn_LongDate (@FullDate DATETIME)
+RETURNS VARCHAR(MAX)
+AS
+BEGIN
+RETURN
+DATENAME(WEEKDAY, @FullDate) + ' ' +
+CAST(DAY(@FullDate) AS VARCHAR(2)) + ' ' +
+DATENAME(MONTH, @FullDate) + ' ' +
+DATENAME(YEAR, @FullDate);
+END;
+
+SELECT name, dbo.fn_LongDate(dob) AS FormattedDOB FROM dbo.Actors;
+
+ALTER FUNCTION dbo.fn_LongDate (@FullDate DATETIME)
+RETURNS VARCHAR(MAX)
+AS
+BEGIN
+RETURN
+DATENAME(WEEKDAY, @FullDate) + ' ' +
+CAST(DAY(@FullDate) AS VARCHAR(2)) +
+CASE
+	WHEN DAY(@FullDate) IN (1, 21, 31) THEN 'st'
+	WHEN DAY(@FullDate) IN (2, 22) THEN 'nd'
+	WHEN DAY(@FullDate) IN (3, 23) THEN 'rd'
+	ELSE 'th'
+END + ' ' +
+DATENAME(MONTH, @FullDate) + ' ' + 
+DATENAME(YEAR, @FullDate);
+END;
+
+
+
 
