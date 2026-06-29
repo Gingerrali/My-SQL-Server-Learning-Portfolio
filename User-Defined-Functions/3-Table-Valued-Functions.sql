4b74cd2d553bcde29a93c3eb0d1f1c9a8146a744
Ralitsa <ralitsa.petkova01@gmail.com>
2026-06-29 17:30:34 +0300

    Update 3-Table-Valued-Functions.sql

--- User-Defined-Functions/3-Table-Valued-Functions.sql
+++ User-Defined-Functions/3-Table-Valued-Functions.sql
@@ -1,1 +1,24 @@
+CREATE OR ALTER FUNCTION fn_SaleStatistics (@CustomerID AS INT)
+RETURNS TABLE
+AS RETURN
+SELECT
+    Orders.Sales.OrderID AS 'Order Number',
+    Orders.Sales.CustomerID AS 'Customer Number',
+    Orders.Customers.CustomerName AS 'Customer Name',
+    Orders.Sales.OrderDate AS 'Order Date',
+    Orders.Sales.ExpectedDeliveryDate AS 'Delivery Date',
+    Orders.OrderLines.OrderLineID AS 'Line Number',
+    Orders.OrderLines.Description AS 'Product Description'
+FROM Orders.Sales
+    INNER JOIN Orders.OrderLines ON Sales.OrderID = OrderLines.OrderID
+    INNER JOIN Orders.Customers ON Sales.CustomerID = Customers.CustomerID
+WHERE Sales.OrderID =
+    (SELECT TOP 1 Sales.OrderID
+     FROM Orders.Sales
+     WHERE Sales.CustomerID = @CustomerID
+     ORDER BY Sales.OrderID DESC)
+;
+GO
 
+SELECT * FROM fn_SaleStatistics(104);
+GO
