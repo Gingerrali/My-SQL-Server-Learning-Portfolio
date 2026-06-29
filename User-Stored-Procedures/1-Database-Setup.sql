a8aac04e708ca1acbf0b54e77380b6ed9cbb77d4
Ralitsa <ralitsa.petkova01@gmail.com>
2026-06-29 23:43:20 +0300

    Update 1-Database-Setup.sql

--- User-Stored-Procedures/1-Database-Setup.sql
+++ User-Stored-Procedures/1-Database-Setup.sql
@@ -1,1 +1,20 @@
+CREATE TABLE dbo.Colours (
+    ColourID INT PRIMARY KEY,
+    ColourName NVARCHAR(20),
+    LastEditedBy INT
+);
+GO
 
+INSERT INTO dbo.Colours (ColourID, ColourName, LastEditedBy)
+VALUES
+(1, 'Azure', 1),
+(2, 'Beige', 1),
+(3, 'Black', 1),
+(4, 'Blue', 1),
+(5, 'Charcoal', 1),
+(6, 'Chartreuse', 1),
+(7, 'Cyan', 1),
+(8, 'Dark Brown', 1),
+(9, 'Dark Green', 1),
+(10, 'Fuchsia', 1);
+GO
