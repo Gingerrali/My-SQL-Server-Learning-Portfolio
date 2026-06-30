ba7c4c1addda5d65c0d6e38f3bb0572d165c46ee
Ralitsa <ralitsa.petkova01@gmail.com>
2026-06-30 14:44:19 +0300

    Create 5-Views.sql

---
+++ 1-Basic-SQL-Queries/5-Views.sql
@@ -0,0 +1,47 @@
+-- Creates a view
+CREATE VIEW Orders.ActivePurchasesView
+AS
+SELECT OrderID, 
+  CustomerID, 
+  OrderDate, 
+  ExpectedDeliveryDate, 
+  CustomerPurchaseOrderNumber
+FROM Orders.Sales
+WHERE SalespersonPersonID > 3;
+GO
+
+-- Displays the view data
+SELECT * FROM Orders.ActivePurchasesView;
+
+-- Alters the view by adding aliases
+ALTER VIEW Orders.ActivePurchasesView
+AS
+SELECT OrderID AS 'System Serial Number', 
+  CustomerID AS 'Customer Number', 
+  OrderDate AS 'Order Date', 
+  ExpectedDeliveryDate AS 'Delivery Date', 
+  CustomerPurchaseOrderNumber AS 'Order Number'
+FROM Orders.Sales
+WHERE SalespersonPersonID > 3;
+GO
+
+-- Adds SCHEMABINDING
+ALTER VIEW Orders.ActivePurchasesView
+WITH SCHEMABINDING
+AS
+SELECT OrderID AS 'System Serial Number', 
+  CustomerID AS 'Customer Number', 
+  OrderDate AS 'Order Date', 
+  ExpectedDeliveryDate AS 'Delivery Date', 
+  CustomerPurchaseOrderNumber AS 'Order Number'
+FROM Orders.Sales
+WHERE SalespersonPersonID > 3;
+GO
+
+-- Attempts to remove a referenced column (should give an error)
+ALTER TABLE Orders.Sales
+DROP COLUMN CustomerID;
+
+-- Deletes the view
+DROP VIEW Orders.ActivePurchasesView;
+GO
