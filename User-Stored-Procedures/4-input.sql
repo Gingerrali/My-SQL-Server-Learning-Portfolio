7cb1e1201b3b5cc4be4116bc11c5ede8269071fb
Ralitsa <ralitsa.petkova01@gmail.com>
2026-06-30 13:20:03 +0300

    Update 4-input.sql

--- User-Stored-Procedures/4-input.sql
+++ User-Stored-Procedures/4-input.sql
@@ -1,3 +1,4 @@
+-- Creates a stored procedure to check if a movie exists in a table
 CREATE PROCEDURE sp_CheckMovieExists @Title VARCHAR(200)
 AS
 BEGIN
@@ -9,11 +10,15 @@ ELSE RETURN 0;
 END;
 GO
 
+-- Tests the procedure with an existing movie title
+-- Returns 1 if the movie exists
 DECLARE @Status INT;
 EXEC @Status = sp_CheckMovieExists 'Psycho';
 SELECT @Status AS MovieExists;
 GO
 
+-- Tests the procedure with a non-existing movie title
+-- Returns 0 if the movie does not exist
 DECLARE @Status INT;
 EXEC @Status = sp_CheckMovieExists '300';
 SELECT @Status AS MovieExists;
