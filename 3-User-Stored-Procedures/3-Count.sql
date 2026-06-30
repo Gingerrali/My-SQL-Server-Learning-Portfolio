2e82307cbe9d9df0207b57c17f0c106aafe9ef72
Ralitsa <ralitsa.petkova01@gmail.com>
2026-06-30 14:52:36 +0300

    Create 3-Count.sql

---
+++ 3-User-Stored-Procedures/3-Count.sql
@@ -0,0 +1,15 @@
+-- Creates a stored procedure that counts table records using an OUTPUT parameter
+CREATE OR ALTER PROCEDURE sp_TotalQuantity (@MinQuantity INT, @OrderLineCount INT OUTPUT)
+AS
+BEGIN
+SELECT @OrderLineCount = COUNT(*) FROM Orders.Orderlines
+WHERE Quantity >= @MinQuantity;
+END;
+GO
+
+-- Executes the procedure with a sample value
+DECLARE @Result INT;
+EXEC sp_TotalQuantity @MinQuantity = 20,
+@OrderLineCount = @Result OUTPUT;
+SELECT @Result AS TotalOrderlines;
+GO
