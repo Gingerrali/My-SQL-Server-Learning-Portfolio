611604cf2771333aaf81b7cab72e972b76da1151
Ralitsa <ralitsa.petkova01@gmail.com>
2026-06-30 13:15:07 +0300

    Update 4-input.sql

--- User-Stored-Procedures/4-input.sql
+++ User-Stored-Procedures/4-input.sql
@@ -1,1 +1,17 @@
+CREATE PROCEDURE sp_CheckMovieExists @Title VARCHAR(200)
+AS
+BEGIN
+IF EXISTS (
+SELECT 1 FROM Movies
+WHERE title = @Title
+) RETURN 1;
+ELSE RETURN 0;
+END;
 
+DECLARE @Status INT;
+EXEC @Status = sp_CheckMovieExists 'Psycho';
+SELECT @Status AS MovieExists;
+
+DECLARE @Status INT;
+EXEC @Status = sp_CheckMovieExists '300';
+SELECT @Status AS MovieExists;
