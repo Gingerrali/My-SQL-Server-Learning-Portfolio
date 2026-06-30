539368ae2568b1322396e6362fec517d15ad78ec
Ralitsa <ralitsa.petkova01@gmail.com>
2026-06-30 11:28:54 +0300

    Update 3-Count.sql

--- User-Stored-Procedures/3-Count.sql
+++ User-Stored-Procedures/3-Count.sql
@@ -1,1 +1,11 @@
+CREATE OR ALTER PROCEDURE sp_TotalQuantity (@MinQuantity INT, @OrderLineCount INT OUTPUT)
+AS
+BEGIN
+SELECT @OrderLineCount = COUNT(*) FROM Orders.Orderlines
+WHERE Quantity >= @MinQuantity;
+END;
 
+DECLARE @Result INT;
+EXEC sp_TotalQuantity @MinQuantity = 20,
+@OrderLineCount = @Result OUTPUT;
+SELECT @Result AS TotalOrderlines;
