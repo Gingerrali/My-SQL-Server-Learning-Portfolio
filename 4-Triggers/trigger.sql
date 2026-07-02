17e8ef9efac90ef92c87ed8f4625e01eecfcaa01
Ralitsa <ralitsa.petkova01@gmail.com>
2026-07-02 11:07:34 +0300

    Update trigger.sql

--- 4-Triggers/trigger.sql
+++ 4-Triggers/trigger.sql
@@ -1,9 +1,11 @@
+-- Creates an audit table to store inserted records
 CREATE TABLE dbo.ColourAudit (
     AuditID INT IDENTITY PRIMARY KEY,
     ColourName nvarchar(20),
     TimeAdded datetime2);
 GO
 
+-- Automatically logs newly inserted records into the audit table
 CREATE TRIGGER dbo.ColourChangeLog
 ON dbo.Colours
 AFTER INSERT
@@ -16,20 +18,24 @@ AS
 ;
 GO
 
+-- Sample inserts to test the trigger
 EXEC dbo.uspInsertColour 'Neon';
 EXEC dbo.uspInsertColour 'Baby Pink';
 GO
 
+-- Verifies that the trigger logged the inserted records
 SELECT * FROM dbo.Colours;
 SELECT * FROM dbo.ColourAudit;
 GO
 
+-- Creates an audit table that tracks stored procedure executions
 CREATE TABLE Orders.SalesAudit (
     AuditID INT IDENTITY PRIMARY KEY,
     CustomerID INT,
     TimeAdded datetime2);
 GO
 
+-- Stored procedure that retrieves customer information and triggers automatic audit logging
 CREATE OR ALTER PROCEDURE Orders.uspHowManyPurchasesPerCustomer (@Customer AS INT)
 AS 
 SELECT CustomerID, CustomerName
@@ -43,9 +49,11 @@ VALUES (@Customer, GETDATE()
 );
 GO
 
+-- Executes the procedure using a sample CustomerID
 EXEC Orders.uspHowManyPurchasesPerCustomer 107;
 GO
-    
+
+-- Checks if the trigger logged the execution successfully
 SELECT * FROM Orders.SalesAudit;
 GO
 
