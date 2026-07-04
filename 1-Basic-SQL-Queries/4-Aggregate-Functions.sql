3a1873391e823fc24edee183c15522e98fe636c4
Ralitsa <ralitsa.petkova01@gmail.com>
2026-07-04 15:39:36 +0300

    Update 4-Aggregate-Functions.sql

--- 1-Basic-SQL-Queries/4-Aggregate-Functions.sql
+++ 1-Basic-SQL-Queries/4-Aggregate-Functions.sql
@@ -1,10 +1,10 @@
--- Calculates a total using SUM()
+-- Calculates a total using SUM().
 SELECT SUM(SalespersonPersonID) AS TotalSales
 FROM Orders.Sales;
 GO
 
--- Counts suppliers per payment period
--- Shows only payment periods used by more than one supplier
+-- Counts suppliers per payment period.
+-- Shows only payment periods used by more than one supplier.
 SELECT PaymentDays,
   COUNT(*) AS TotalSuppliers
 FROM Orders.Suppliers
@@ -13,7 +13,7 @@ HAVING COUNT(*) > 1
 ORDER BY TotalSuppliers DESC;
 GO
 
--- MAX, MIN, AVG aggregate functions example
+-- MAX, MIN, AVG aggregate functions example.
 SELECT
     MAX(PaymentDays) AS LongestPeriodRemaining,
     MIN(PaymentDays) AS ShortestPeriodRemaining,
