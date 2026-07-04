da75a8d092cbe6f9939433062cdac74754c949f5
Ralitsa <ralitsa.petkova01@gmail.com>
2026-07-04 15:45:34 +0300

    Update 3-Transaction-Transfer.sql

--- 5-Transactions/3-Transaction-Transfer.sql
+++ 5-Transactions/3-Transaction-Transfer.sql
@@ -1,4 +1,4 @@
--- Creates a table with sample data for the transaction example
+-- Creates a table with sample data for the transaction example.
 CREATE TABLE dbo.BankAccounts (
     AccountID INT PRIMARY KEY,
     Balance decimal(10,2)
@@ -7,12 +7,12 @@ INSERT INTO dbo.BankAccounts
 VALUES (1, 100.00), (2, 200.00), (3, 300.00);
 GO
 
--- Shows the initial account balances
+-- Shows the initial account balances.
 SELECT * FROM dbo.BankAccounts;
 GO
 
--- Creates a stored procedure that transfers funds between two accounts
--- The transaction updates both account balances as a single operationn 
+-- Creates a stored procedure that transfers funds between two accounts.
+-- The transaction updates both account balances as a single operationn.
 CREATE OR ALTER PROCEDURE dbo.TransferFunds (@FromAccount AS INT, @ToAccount AS INT, @Amount AS decimal(10,2))
 AS
 BEGIN TRANSACTION;
@@ -24,6 +24,6 @@ UPDATE dbo.BankAccounts
     WHERE AccountID = @ToAccount;
 COMMIT TRANSACTION;
 
--- Executes the transaction and displays the updated balances
+-- Executes the transaction and displays the updated balances.
 EXEC dbo.TransferFunds 1, 3, 50.00;
 SELECT * FROM dbo.BankAccounts;
