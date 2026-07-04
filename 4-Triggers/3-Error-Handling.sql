559f72dfdc70d422c2dbc395337c2e793fb01704
Ralitsa <ralitsa.petkova01@gmail.com>
2026-07-04 15:32:57 +0300

    Update 3-Error-Handling.sql

--- 4-Triggers/3-Error-Handling.sql
+++ 4-Triggers/3-Error-Handling.sql
@@ -1,3 +1,8 @@
+-- Demonstrates error handling using TRY and CATCH
+-- Attempts to insert a new record
+-- Error 515 is a system-defined error for NULL values in NOT NULL columns
+-- Displays a custom message instead of the default system error
+-- Returns a generic message for all other errors
 BEGIN TRY
 INSERT INTO Orders.Suppliers (SupplierName) VALUES ('The Walt Disney Company');
 END TRY
