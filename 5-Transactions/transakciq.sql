f53e3022ad2c97f21a215327c274dcba5fff2e5c
Ralitsa <ralitsa.petkova01@gmail.com>
2026-07-04 14:48:52 +0300

    Update transakciq.sql

--- 5-Transactions/transakciq.sql
+++ 5-Transactions/transakciq.sql
@@ -1,16 +1,17 @@
+-- Creates a table with sample data, used for the transaction example
 CREATE TABLE dbo.BankAccounts (
     AccountID INT PRIMARY KEY,
     Balance decimal(10,2)
 );
-GO
-
 INSERT INTO dbo.BankAccounts
 VALUES (1, 100.00), (2, 200.00), (3, 300.00);
 GO
-
+    
 SELECT * FROM dbo.BankAccounts;
 GO
 
+-- Creates a stored procedure that transfers funds between two accounts
+-- Starts a transaction so multiple UPDATE statements are executed as a single unit
 CREATE OR ALTER PROCEDURE dbo.TransferFunds (@FromAccount AS INT, @ToAccount AS INT, @Amount AS decimal(10,2))
 AS
 BEGIN TRANSACTION;
@@ -22,5 +23,6 @@ UPDATE dbo.BankAccounts
     WHERE AccountID = @ToAccount;
 COMMIT TRANSACTION;
 
+-- Executes the stored procedure with sample values and displays the final result
 EXEC dbo.TransferFunds 1, 3, 50.00;
 SELECT * FROM dbo.BankAccounts;
