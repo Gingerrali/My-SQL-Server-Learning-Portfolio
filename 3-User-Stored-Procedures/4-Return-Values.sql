926956717e3e9140dc66f6b4930aa6988846cf13
Ralitsa <ralitsa.petkova01@gmail.com>
2026-07-04 15:43:01 +0300

    Update 4-Return-Values.sql

--- 3-User-Stored-Procedures/4-Return-Values.sql
+++ 3-User-Stored-Procedures/4-Return-Values.sql
@@ -1,4 +1,4 @@
--- Creates a stored procedure to check if a movie exists in a table
+-- Creates a stored procedure to check if a movie exists in a table.
 CREATE PROCEDURE sp_CheckMovieExists @Title VARCHAR(200)
 AS
 BEGIN
@@ -10,15 +10,15 @@ ELSE RETURN 0;
 END;
 GO
 
--- Tests the procedure with an existing movie title
--- Returns 1 if the movie exists
+-- Tests the procedure with an existing movie title.
+-- Returns 1 if the movie exists.
 DECLARE @Status INT;
 EXEC @Status = sp_CheckMovieExists 'Psycho';
 SELECT @Status AS MovieExists;
 GO
 
--- Tests the procedure with a non-existing movie title
--- Returns 0 if the movie does not exist
+-- Tests the procedure with a non-existing movie title.
+-- Returns 0 if the movie does not exist.
 DECLARE @Status INT;
 EXEC @Status = sp_CheckMovieExists '300';
 SELECT @Status AS MovieExists;
