65180f65d25cf49362f9af98d90cb762b60b45d2
Ralitsa <ralitsa.petkova01@gmail.com>
2026-07-04 15:42:21 +0300

    Update 2-Input-Parameters.sql

--- 3-User-Stored-Procedures/2-Input-Parameters.sql
+++ 3-User-Stored-Procedures/2-Input-Parameters.sql
@@ -1,4 +1,4 @@
--- Creates a stored procedure that inserts a new record using an INPUT parameter
+-- Creates a stored procedure that inserts a new record using an INPUT parameter.
 CREATE OR ALTER PROCEDURE dbo.uspInsertColour (@Colour AS nvarchar(100))
 AS
     DECLARE @ColourID INT
@@ -11,16 +11,16 @@ AS
 ;
 GO
 
--- Executes the procedure with sample INPUT values
+-- Executes the procedure with sample INPUT values.
 EXEC dbo.uspInsertColour 'Neon';
 EXEC dbo.uspInsertColour 'Baby Pink';
 GO
 
--- Checks the updated table
+-- Checks the updated table.
 SELECT * FROM dbo.Colours;
 GO
 
--- Creates a new stored procedure that removes the last record
+-- Creates a new stored procedure that removes the last record.
 CREATE OR ALTER PROCEDURE dbo.uspRemoveLastColour
 AS
     DELETE FROM dbo.Colours
