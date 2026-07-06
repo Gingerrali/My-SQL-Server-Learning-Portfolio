822afeda5ea1583cc4e2584b69e4e48151745c9b
Ralitsa <ralitsa.petkova01@gmail.com>
2026-07-06 18:43:59 +0300

    Update 4-NULL-Values.sql

--- 7-Advanced-SQL/4-NULL-Values.sql
+++ 7-Advanced-SQL/4-NULL-Values.sql
@@ -1,3 +1,4 @@
+-- Uses DEFAULT values when no value is provided for a NOT NULL column.
 CREATE TABLE Birthdays (
     PersonName nvarchar(100) NOT NULL,
     Birthday date NOT NULL DEFAULT '1/1/1111')
@@ -7,15 +8,22 @@ INSERT INTO Birthdays (PersonName, Birthday)
 VALUES ('Jin', '5/17/1982');
 GO
 
+-- Displays the inserted data and deletes the table.
 SELECT * FROM Birthdays;
 DROP TABLE Birthdays;
+GO
 
-
-
-
-Тук, някои от показателите в DeliveryMethodID са NULL стойности, за да ги заменим със стойност 1 примерно, пишем: 
+-- Uses ISNULL to replace NULL values with 1.
 SELECT SupplierID,
        SupplierName,
        ISNULL(DeliveryMethodID, 1)
 FROM Orders.Suppliers;
 GO
+
+-- Replaces NULL values with a default text.
+SELECT Customers.CustomerName,
+       Customers.CustomerCategoryID,
+       ISNULL(Sales.Comments, 'No Comments') AS Comments
+FROM Orders.Customers
+    LEFT JOIN Orders.Sales
+    ON Customers.CustomerID = Sales.CustomerID;
