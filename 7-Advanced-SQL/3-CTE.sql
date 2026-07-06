42cb5c1e970db9da60e04e3b55f2d08234771749
Ralitsa <ralitsa.petkova01@gmail.com>
2026-07-06 16:46:35 +0300

    Update 3-CTE.sql

--- 7-Advanced-SQL/3-CTE.sql
+++ 7-Advanced-SQL/3-CTE.sql
@@ -1,1 +1,16 @@
-
+-- Creates CTEs (temporary tables) used only within a query.
+-- This example joins two CTEs to combine their data.
+WITH EarlyFilms AS (
+SELECT director_id, title, year 
+FROM dbo.Movies
+WHERE year < 2000
+),
+Recentfilms AS (SELECT director_id, title, year 
+FROM dbo.Movies
+WHERE year >= 2000
+)
+SELECT d.name AS Director, e.title AS EarlyFilm, e.year AS EarlyYear, r.title AS RecentFilm, r.year AS RecentYear
+FROM EarlyFilms e
+JOIN RecentFilms r ON e.director_id = r.director_id
+JOIN dbo.Directors d ON e.director_id = d.director_id;
+GO
