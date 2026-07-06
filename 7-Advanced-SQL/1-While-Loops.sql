8039c43fb3606bf32a5e5b0b6267ab81c9ad6dfd
Ralitsa <ralitsa.petkova01@gmail.com>
2026-07-06 12:47:06 +0300

    Update 1-While-Loops.sql

--- 7-Advanced-SQL/1-While-Loops.sql
+++ 7-Advanced-SQL/1-While-Loops.sql
@@ -1,1 +1,11 @@
-
+-- Uses a WHILE loop to count movies for each rating.
+DECLARE @Rating INT = 1;
+DECLARE @Count INT;
+WHILE @Rating <= 5
+BEGIN
+SELECT @Count = COUNT(*)
+FROM dbo.Movies
+WHERE rating = @Rating;
+SELECT CONCAT('Rating: ', @Count, ' movies') AS RatingScore;
+SET @Rating = @Rating + 1;
+END
