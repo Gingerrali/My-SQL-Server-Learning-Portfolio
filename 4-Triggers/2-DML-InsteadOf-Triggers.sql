08e016b075581af69d9de2dd1974ebff3f23fb30
Ralitsa <ralitsa.petkova01@gmail.com>
2026-07-07 12:46:00 +0300

    Update 2-DML-InsteadOf-Triggers.sql

--- 4-Triggers/2-DML-InsteadOf-Triggers.sql
+++ 4-Triggers/2-DML-InsteadOf-Triggers.sql
@@ -1,1 +1,36 @@
+-- Creates the main and archive tables used by the trigger.
+CREATE TABLE dbo.Vendors (
+    VendorID int IDENTITY PRIMARY KEY,
+    VendorName nvarchar(50)
+);
+CREATE TABLE dbo.VendorArchive (
+    VendorID int PRIMARY KEY,
+    VendorName nvarchar(50),
+    DateArchived datetime2
+);
+INSERT INTO dbo.Vendors
+VALUES ('KinetEco');
+SELECT * FROM dbo.Vendors;
+GO
 
+-- Uses an INSTEAD OF trigger to save deleted records in an archive table.
+CREATE TRIGGER dbo.ArchiveVendors
+ON dbo.Vendors
+AFTER DELETE
+AS
+SET NOCOUNT ON;
+INSERT INTO VendorArchive (VendorID, VendorName)
+    SELECT deleted.VendorID, deleted.VendorName
+    FROM deleted;
+UPDATE VendorArchive
+    SET DateArchived = GETDATE()
+    FROM deleted
+    WHERE VendorArchive.VendorID = deleted.VendorID;
+;
+GO
+
+DELETE FROM dbo.Vendors
+WHERE VendorID = 1;
+SELECT * FROM dbo.Vendors;
+SELECT * FROM dbo.VendorArchive;
+GO
