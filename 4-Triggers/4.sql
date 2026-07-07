d48a8aa5c2746e26098964349d72b8aec6b82488
Ralitsa <ralitsa.petkova01@gmail.com>
2026-07-07 15:05:11 +0300

    Update 4.sql

--- 4-Triggers/4.sql
+++ 4-Triggers/4.sql
@@ -56,3 +56,31 @@ CREATE TABLE dbo.TestTable (
 SELECT * FROM dbo.TestTable;
 SELECT * FROM dbo.TableEventCapture;
 GO
+
+
+
+-- 
+CREATE TABLE dbo.Table2EventCapture (
+    EventID int IDENTITY PRIMARY KEY,
+    EventTime datetime2,
+    EventType nvarchar(100),
+    UserName nvarchar(100),
+    ObjectName nvarchar(100),
+    TSQLCommand nvarchar(2000)
+);
+GO
+
+CREATE OR ALTER TRIGGER TableEventTrigger
+ON DATABASE
+FOR DDL_TABLE_EVENTS
+AS
+INSERT dbo.Table2EventCapture (
+    EventTime, EventType, UserName, ObjectName, TSQLCommand)
+VALUES (
+    GETDATE(),
+    EVENTDATA().value('(/EVENT_INSTANCE/EventType)[1]', 'nvarchar(100)'),
+    EVENTDATA().value('(/EVENT_INSTANCE/UserName)[1]', 'nvarchar(100)'),
+    EVENTDATA().value('(/EVENT_INSTANCE/ObjectName)[1]', 'nvarchar(100)'),
+    EVENTDATA().value('(/EVENT_INSTANCE/TSQLCommand)[1]', 'nvarchar(2000)')
+);
+GO
