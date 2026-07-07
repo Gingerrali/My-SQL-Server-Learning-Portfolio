38efe5142f5c5b9afb8e8058a55ba91a9f356fc4
Ralitsa <ralitsa.petkova01@gmail.com>
2026-07-07 12:47:17 +0300

    Update 1-DML-After-Triggers.sql

--- 4-Triggers/1-DML-After-Triggers.sql
+++ 4-Triggers/1-DML-After-Triggers.sql
@@ -28,6 +28,8 @@ SELECT * FROM dbo.Colours;
 SELECT * FROM dbo.ColourAudit;
 GO
 
+
+    
 -- Creates an audit table that tracks stored procedure executions.
 CREATE TABLE Orders.SalesAudit (
     AuditID INT IDENTITY PRIMARY KEY,
@@ -57,3 +59,41 @@ GO
 SELECT * FROM Orders.SalesAudit;
 GO
 
+
+    
+-- Creates the main and archive tables used by the trigger.
+CREATE TABLE dbo.Vendors (
+    VendorID int IDENTITY PRIMARY KEY,
+    VendorName nvarchar(50)
+);
+CREATE TABLE dbo.VendorArchive (
+    VendorID int PRIMARY KEY,
+    VendorName nvarchar(50),
+    DateArchived datetime2
+);
+INSERT INTO dbo.Vendors
+VALUES ('KinetEco');
+SELECT * FROM dbo.Vendors;
+GO
+
+-- Uses an INSTEAD OF trigger to save deleted records in an archive table.
+CREATE TRIGGER dbo.ArchiveVendors
+ON dbo.Vendors
+AFTER DELETE
+AS
+SET NOCOUNT ON;
+INSERT INTO VendorArchive (VendorID, VendorName)
+    SELECT deleted.VendorID, deleted.VendorName
+    FROM deleted;
+UPDATE VendorArchive
+    SET DateArchived = GETDATE()
+    FROM deleted
+    WHERE VendorArchive.VendorID = deleted.VendorID;
+;
+GO
+
+DELETE FROM dbo.Vendors
+WHERE VendorID = 1;
+SELECT * FROM dbo.Vendors;
+SELECT * FROM dbo.VendorArchive;
+GO
