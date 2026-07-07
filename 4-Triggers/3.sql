bc5a43c31564d114ad017bb864f71cd2f38c7bd2
Ralitsa <ralitsa.petkova01@gmail.com>
2026-07-07 13:58:29 +0300

    Create 3.sql

---
+++ 4-Triggers/3.sql
@@ -0,0 +1,18 @@
+CREATE OR ALTER TRIGGER PreventTableChanges
+ON DATABASE
+FOR ALTER_TABLE
+AS
+PRINT 'A trigger is cancelling all ALTER TABLE statements.';
+ROLLBACK;
+GO
+
+CREATE OR ALTER TRIGGER PreventTableChanges
+ON ALL SERVER
+FOR DDL_TABLE_EVENTS
+AS
+PRINT 'A trigger is cancelling all ALTER TABLE statements.';
+ROLLBACK;
+GO
+
+DROP TRIGGER PreventTableChanges
+ON ALL SERVER;
