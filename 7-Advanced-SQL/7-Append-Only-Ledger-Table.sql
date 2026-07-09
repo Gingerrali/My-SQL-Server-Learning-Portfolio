f2d75c2ef9f0b7cab8f0b8cd0eb1de7d6f48f014
Ralitsa <ralitsa.petkova01@gmail.com>
2026-07-09 18:31:29 +0300

    Update 7-Append-Only-Ledger-Table.sql

--- 7-Advanced-SQL/7-Append-Only-Ledger-Table.sql
+++ 7-Advanced-SQL/7-Append-Only-Ledger-Table.sql
@@ -1,3 +1,5 @@
+-- Creates an append-only ledger table where existing records
+-- cannot be changed or deleted.
 CREATE TABLE Orders.TaxID (
     CustomerID INT NOT NULL PRIMARY KEY CLUSTERED,
     TaxID VARCHAR (50) NOT NULL)
@@ -7,6 +9,7 @@ WITH
 );
 GO
 
+-- Adds sample records to the table.    
 INSERT INTO Orders.TaxID VALUES
 (1, '000-12-3456'),
 (2, '123-45-6789'),
@@ -14,17 +17,19 @@ INSERT INTO Orders.TaxID VALUES
 (4, '001-02-0345');
 GO
 
+-- Attempts to update a record, which is not allowed
+-- in an append-only ledger table, and returns an error.
 UPDATE Orders.TaxID
 SET TaxID = '999-99-9999'
 WHERE CustomerID = 3;
 GO
 
+-- Attempts to delete a record, which also returns an error.
 DELETE FROM Orders.TaxID					    
 WHERE CustomerID = 3;
 GO
 
+-- Attempts to drop the table. The table appears to be deleted, but SQL server
+-- automatically archives it in Dropped Ledger Tables.
 DROP TABLE Orders.TaxID;
 GO
-
-ако опитаме да изтрием цялата таблица, ще изглежда изтрита, но ще всъщност ще се премести в архив 
-(в Drpped Ledger Tables)
