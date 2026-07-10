c7590122a149faf452c30ad78d376e26a148282b
Ralitsa <ralitsa.petkova01@gmail.com>
2026-07-10 22:16:28 +0300

    Update 4-Temporary-Tables.sql

--- 7-Advanced-SQL/4-Temporary-Tables.sql
+++ 7-Advanced-SQL/4-Temporary-Tables.sql
@@ -1,1 +1,16 @@
+-- Local tables are temporary and visible in only one session and marked with '#'. 
+-- '##' suggests a ... table and gives access to all with access to the server.
+-- The table below is an example of a local table that sorts movies under highest revenue.
+CREATE TABLE #MovieRevenues (
+    Movie_id INT, Title VARCHAR(200), RevenueGenerated INT
+);
+GO
 
+INSERT INTO #MovieRevenues (Movie_id, Title, RevenueGenerated)
+SELECT movie_id, title, revenue_generated FROM dbo.Movies
+ORDER BY revenue_generated DESC;
+GO
+
+SELECT TOP 10  * FROM #MovieRevenues
+ORDER BY RevenueGenerated DESC;
+GO
