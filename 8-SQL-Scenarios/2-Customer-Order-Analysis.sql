1d2694bed40a3b8595373c79ada9f375851a8d4c
Ralitsa <ralitsa.petkova01@gmail.com>
2026-09-20 13:33:55 +0300

    Update 2-Customer-Order-Analysis.sql

--- 8-SQL-Scenarios/2-Customer-Order-Analysis.sql
+++ 8-SQL-Scenarios/2-Customer-Order-Analysis.sql
@@ -2,3 +2,5 @@
 -- The company needs a customer order analysis to identify
 -- the most active customers and those generating the highest revenue,
 -- then to categorize customers based on their total revenue.
+
+-- 
