0a5c53fdee3d5a1e3f8b2e6af4be5a3b75709532
Ralitsa <ralitsa.petkova01@gmail.com>
2026-09-20 13:57:59 +0300

    Update 2-Customer-Order-Analysis.sql

--- 8-SQL-Scenarios/2-Customer-Order-Analysis.sql
+++ 8-SQL-Scenarios/2-Customer-Order-Analysis.sql
@@ -4,3 +4,16 @@
 -- then to categorize customers based on their total revenue.
 
 -- Creates a query that analyzes customers' total orders, purchased quantities, and total revenue.
+SELECT Customers.CustomerID, 
+CustomerName AS Name, 
+COUNT(DISTINCT Orders.OrderID) AS TotalOrders,
+SUM(Orderlines.Quantity) AS TotalQuantityPurchased,
+SUM((Orderlines.Quantity)*(Orderlines.UnitPrice)) AS TotalRevenue
+FROM Sales.Customers
+INNER JOIN Sales.Orders
+ON Sales.Customers.CustomerID = Sales.Orders.CustomerID
+INNER JOIN Sales.OrderLines
+ON Sales.Orders.OrderID = Sales.Orderlines.OrderID
+GROUP BY Customers.CustomerID,
+Customers.CustomerName
+ORDER BY TotalRevenue DESC;
