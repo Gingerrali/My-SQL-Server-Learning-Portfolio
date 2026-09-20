b6ee901731933aa6e1e999333dc3e50cb798382b
Ralitsa <ralitsa.petkova01@gmail.com>
2026-09-20 13:37:49 +0300

    Update 2-Customer-Order-Analysis.sql

--- 8-SQL-Scenarios/2-Customer-Order-Analysis.sql
+++ 8-SQL-Scenarios/2-Customer-Order-Analysis.sql
@@ -3,4 +3,4 @@
 -- the most active customers and those generating the highest revenue,
 -- then to categorize customers based on their total revenue.
 
--- 
+-- Creates a query that analyzes customers' total orders, purchased quantities, and total revenue.
