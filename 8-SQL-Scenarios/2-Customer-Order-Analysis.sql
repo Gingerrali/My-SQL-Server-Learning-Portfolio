a7dcb27bae6a992c50638830a633e52b87d051b6
Ralitsa <ralitsa.petkova01@gmail.com>
2026-09-20 14:04:32 +0300

    Update 2-Customer-Order-Analysis.sql

--- 8-SQL-Scenarios/2-Customer-Order-Analysis.sql
+++ 8-SQL-Scenarios/2-Customer-Order-Analysis.sql
@@ -3,17 +3,24 @@
 -- the most active customers and those generating the highest revenue,
 -- then to categorize customers based on their total revenue.
 
+-- Displays the tables used for the analysis
+SELECT * FROM Sales.Customers;
+SELECT * FROM Sales.Orders;
+SELECT * FROM Sales.OrderLines;
+GO
+
 -- Creates a query that analyzes customers' total orders, purchased quantities, and total revenue.
 SELECT Customers.CustomerID, 
-CustomerName AS Name, 
-COUNT(DISTINCT Orders.OrderID) AS TotalOrders,
-SUM(Orderlines.Quantity) AS TotalQuantityPurchased,
-SUM((Orderlines.Quantity)*(Orderlines.UnitPrice)) AS TotalRevenue
+  CustomerName AS Name, 
+  COUNT(DISTINCT Orders.OrderID) AS TotalOrders,
+  SUM(Orderlines.Quantity) AS TotalQuantityPurchased,
+  SUM((Orderlines.Quantity)*(Orderlines.UnitPrice)) AS TotalRevenue
 FROM Sales.Customers
 INNER JOIN Sales.Orders
-ON Sales.Customers.CustomerID = Sales.Orders.CustomerID
+  ON Sales.Customers.CustomerID = Sales.Orders.CustomerID
 INNER JOIN Sales.OrderLines
-ON Sales.Orders.OrderID = Sales.Orderlines.OrderID
+  ON Sales.Orders.OrderID = Sales.Orderlines.OrderID
 GROUP BY Customers.CustomerID,
-Customers.CustomerName
+  Customers.CustomerName
 ORDER BY TotalRevenue DESC;
+GO
