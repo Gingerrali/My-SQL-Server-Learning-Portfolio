584d34ddb2b4387ad4fa1740865642af74b66308
Ralitsa <ralitsa.petkova01@gmail.com>
2026-09-23 22:27:49 +0300

    Update 2-Customer-Order-Analysis.sql

--- 8-SQL-Scenarios/2-Customer-Order-Analysis.sql
+++ 8-SQL-Scenarios/2-Customer-Order-Analysis.sql
@@ -43,4 +43,9 @@ AND SUM(Orderlines.Quantity * Orderlines.UnitPrice) > 5000
 ORDER BY TotalRevenue DESC;
 
 -- Uses a CTE to calculate each customer's total orders, purchased quantities, and total revenue,
--- then joins the result with the Customers table to display the customer name and assign a revenue category.
+-- then joins the result with the Customers table to display the customer name.
+-- Categorizes customers based on their total revenue.
+
+
+
+
